aae047427379027852f642cfdab1d21ffd2e192c
dblanutsa <dmitriy.blanutsa@gmail.com>
2018-05-30 19:09:08 +0300

    add db postgres

--- src/main/java/activemqservice/Application.java
+++ src/main/java/activemqservice/Application.java
@@ -3,9 +3,11 @@ package activemqservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJms
+@EnableTransactionManagement
 public class Application {
 
     public static void main(String[] args) {
